5ca32836ac270882d8710a106abb9ad883e156d4
Java-Terry <110506534+Java-Terry@users.noreply.github.com>
2024-10-29 10:55:15 +1000

    Create plot_traject.py

---
+++ plot_traject.py
@@ -0,0 +1,166 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+def precision(frame_number, ground_truth):
+    return len(set(frame_number).intersection(ground_truth)) / len(frame_number)
+
+def recall(frame_number, ground_truth):
+    return len(set(frame_number).intersection(ground_truth)) / len(ground_truth)
+
+##orbslam normal
+#key_trajectoryfile = "/home/java/results-anyFeature/test/KITTI/05/00003_KeyFrameTrajectory.txt"
+#system_outputfile = "/home/java/results-anyFeature/test/KITTI/05/system_output_00003.txt"
+key_trajectoryfile = "/home/java/results-anyFeature/test/KITTI/08/00001_KeyFrameTrajectory.txt"
+system_outputfile = "/home/java/results-anyFeature/test/KITTI/08/system_output_00001.txt"
+gnd_truth_05 = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614, 615, 616, 617, 618, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 640, 641, 642, 643, 644, 645, 646, 647, 648, 649, 650, 651, 652, 653, 654, 655, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 683, 684, 685, 686, 687, 688, 689, 690, 691, 692, 693, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 725, 726, 727, 728, 729, 730, 731, 732, 733, 734, 735, 736, 737, 738, 739, 740, 741, 742, 743, 744, 745, 746, 747, 748, 749, 750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762, 763, 764, 765, 766, 767, 768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806, 807, 808, 809, 810, 811, 812, 813, 814, 815, 816, 817, 818, 819, 820, 821, 822, 823, 824, 825, 826, 827, 828, 829, 830, 831, 832, 833, 834, 835, 836, 837, 838, 839, 840, 841, 842, 843, 844, 845, 846, 847, 848, 849, 850, 851, 852, 853, 854, 855, 856, 857, 858, 859, 860, 861, 862, 863, 864, 865, 866, 867, 868, 869, 870, 871, 872, 873, 874, 875, 876, 877, 878, 879, 880, 881, 882, 883, 884, 885, 886, 887, 888, 889, 890, 891, 892, 893, 894, 895, 896, 897, 898, 899, 900, 901, 902, 903, 904, 905, 906, 907, 908, 909, 1533, 1534, 1535, 1536, 1537, 1538, 1539, 1540, 1541, 1542, 1543, 1544, 1545, 1546, 1547, 1548, 1549, 1550, 1551, 1552, 1553, 1554, 1555, 1556, 1557, 1558, 1559, 1560, 1561, 1562, 1563, 1564, 1565, 1566, 1567, 1568]
+gnd_truth = [67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 2491, 2492, 2493, 2494, 2495, 2496, 2497, 2498, 2499, 2500, 2501, 2502, 2503, 2504, 2505, 2506, 2507, 2508, 2509, 2510, 2511, 2512, 2513, 2514, 2515, 2516, 2517, 2518, 2519, 2520, 2521, 2522, 2523, 2524, 2525, 2526, 2527, 2528, 2529, 2530, 2531, 2532, 2533, 2534, 2535, 2536, 2537, 2538, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 725, 726, 727, 728, 729, 730, 731, 732, 733, 734, 735, 736, 737, 738, 739, 740, 741, 742, 743, 744, 745, 746, 747, 748, 749, 750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762, 763, 764, 765, 766, 767, 768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805]
+
+data = np.loadtxt(key_trajectoryfile)
+key_id = data[:, 0]
+timestamps = data[:, 1]
+translations = data[:, 2:5]
+key_trajectory_data = {}
+with open(key_trajectoryfile, 'r') as file:
+    for line in file:
+        parts = line.split()
+        frame_id = int(parts[0])
+        translation = [float(x) for x in parts[2:5]]  # Assuming translations are the second, third, and fourth values
+        key_trajectory_data[frame_id] = translation
+
+
+frame_numbers = []
+with open(system_outputfile, 'r') as file:
+    for line in file:
+        match = re.search(r'DetectLoop done and true at: (\d+)', line)
+        if match:
+            frame_numbers.append(int(match.group(1)))
+
+loop_closed = []
+with open(system_outputfile, 'r') as file:
+    for line in file:
+        match = re.search(r'Loop Closure at: (\d+)', line)
+        if match:
+            loop_closed.append(int(match.group(1)))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot trajectory
+ax.plot(translations[:, 0], translations[:, 2], -translations[:, 1], linestyle='-', linewidth=0.5)
+
+
+for frame_number in frame_numbers:
+    if frame_number in key_trajectory_data:
+        translation = key_trajectory_data[frame_number]
+        ax.scatter(translation[0], translation[2], -translation[1], color='red', s=10, marker='o')
+for gnd in gnd_truth:
+    if gnd in key_trajectory_data:
+        translation = key_trajectory_data[gnd]
+        ax.scatter(translation[0], translation[2], -translation[1], color='green', s=10, marker='+')
+    else: # Find the nearest frame ID in key_trajectory_data
+        nearest_frame_id = min(key_trajectory_data.keys(), key=lambda x: abs(x - gnd))
+        translation = key_trajectory_data[nearest_frame_id]
+        ax.scatter(translation[0], translation[2], -translation[1], color='green', s=10, marker='+')
+for loop in loop_closed:
+    if loop in key_trajectory_data:
+        translation = key_trajectory_data[loop]
+        ax.scatter(translation[0], translation[2], -translation[1], color='blue', s=30, marker='x')
+    else: # Find the nearest frame ID in key_trajectory_data
+        nearest_frame_id = min(key_trajectory_data.keys(), key=lambda x: abs(x - loop))
+        translation = key_trajectory_data[nearest_frame_id]
+        ax.scatter(translation[0], translation[2], -translation[1], color='blue', s=30, marker='x')
+
+# Set labels and title
+ax.set_xlabel('X')
+ax.set_ylabel('Z')
+ax.set_zlabel('Y')
+ax.set_title('Keyframe Trajectory')
+
+# # Show plot
+# plt.show()
+
+# ##FOR VPR MIX
+# key_trajectoryfile = "/home/java/results-anyFeature/test/KITTI/05/00005_KeyFrameTrajectory.txt"
+# system_outputfile = "/home/java/results-anyFeature/test/KITTI/05/system_output_00005.txt"
+# #key_trajectoryfile = "/home/java/results-anyFeature/test/KITTI/08/00002_KeyFrameTrajectory.txt"
+# #system_outputfile = "/home/java/results-anyFeature/test/KITTI/08/system_output_00002.txt"
+# gnd_truth_05 = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614, 615, 616, 617, 618, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 640, 641, 642, 643, 644, 645, 646, 647, 648, 649, 650, 651, 652, 653, 654, 655, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 683, 684, 685, 686, 687, 688, 689, 690, 691, 692, 693, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 725, 726, 727, 728, 729, 730, 731, 732, 733, 734, 735, 736, 737, 738, 739, 740, 741, 742, 743, 744, 745, 746, 747, 748, 749, 750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762, 763, 764, 765, 766, 767, 768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806, 807, 808, 809, 810, 811, 812, 813, 814, 815, 816, 817, 818, 819, 820, 821, 822, 823, 824, 825, 826, 827, 828, 829, 830, 831, 832, 833, 834, 835, 836, 837, 838, 839, 840, 841, 842, 843, 844, 845, 846, 847, 848, 849, 850, 851, 852, 853, 854, 855, 856, 857, 858, 859, 860, 861, 862, 863, 864, 865, 866, 867, 868, 869, 870, 871, 872, 873, 874, 875, 876, 877, 878, 879, 880, 881, 882, 883, 884, 885, 886, 887, 888, 889, 890, 891, 892, 893, 894, 895, 896, 897, 898, 899, 900, 901, 902, 903, 904, 905, 906, 907, 908, 909, 1533, 1534, 1535, 1536, 1537, 1538, 1539, 1540, 1541, 1542, 1543, 1544, 1545, 1546, 1547, 1548, 1549, 1550, 1551, 1552, 1553, 1554, 1555, 1556, 1557, 1558, 1559, 1560, 1561, 1562, 1563, 1564, 1565, 1566, 1567, 1568]
+# gnd_truth = [67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 2491, 2492, 2493, 2494, 2495, 2496, 2497, 2498, 2499, 2500, 2501, 2502, 2503, 2504, 2505, 2506, 2507, 2508, 2509, 2510, 2511, 2512, 2513, 2514, 2515, 2516, 2517, 2518, 2519, 2520, 2521, 2522, 2523, 2524, 2525, 2526, 2527, 2528, 2529, 2530, 2531, 2532, 2533, 2534, 2535, 2536, 2537, 2538, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 725, 726, 727, 728, 729, 730, 731, 732, 733, 734, 735, 736, 737, 738, 739, 740, 741, 742, 743, 744, 745, 746, 747, 748, 749, 750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762, 763, 764, 765, 766, 767, 768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805]
+
+
+# data = np.loadtxt(key_trajectoryfile)
+# key_id = data[:, 0]
+# timestamps = data[:, 1]
+# translations = data[:, 2:5]
+# key_trajectory_data = {}
+# with open(key_trajectoryfile, 'r') as file:
+#     for line in file:
+#         parts = line.split()
+#         frame_id = int(parts[0])
+#         translation = [float(x) for x in parts[2:5]]  # Assuming translations are the second, third, and fourth values
+#         key_trajectory_data[frame_id] = translation
+
+
+# frame_numbers = []
+# with open(system_outputfile, 'r') as file:
+#     for line in file:
+#         match = re.search(r'Frame ID: (\d+) loop_candidates: (.+)', line)
+#         if match:
+#             frame_id = int(match.group(1))
+#             loop_candidates_str = match.group(2)
+#             loop_candidates = [int(x) for x in loop_candidates_str.split()]
+#             for i in loop_candidates:
+#                 frame_numbers.append(i)
+
+# loop_closed = []
+# with open(system_outputfile, 'r') as file:
+#     for line in file:
+#         match = re.search(r'Loop Closure at: (\d+)', line)
+#         if match:
+#             loop_closed.append(int(match.group(1)))
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+
+# # Plot trajectory
+# ax.plot(translations[:, 0], translations[:, 2], -translations[:, 1], linestyle='-', linewidth=0.5)
+
+
+# for frame_number in frame_numbers:
+#     if frame_number in key_trajectory_data:
+#         translation = key_trajectory_data[frame_number]
+#         ax.scatter(translation[0], translation[2], -translation[1], color='red', s=10, marker='o')
+# for gnd in gnd_truth:
+#     if gnd in key_trajectory_data:
+#         translation = key_trajectory_data[gnd]
+#         ax.scatter(translation[0], translation[2], -translation[1], color='green', s=10, marker='+')
+#     else: # Find the nearest frame ID in key_trajectory_data
+#         nearest_frame_id = min(key_trajectory_data.keys(), key=lambda x: abs(x - gnd))
+#         translation = key_trajectory_data[nearest_frame_id]
+#         ax.scatter(translation[0], translation[2], -translation[1], color='green', s=10, marker='+')
+# for loop in loop_closed:
+#     if loop in key_trajectory_data:
+#         translation = key_trajectory_data[loop]
+#         ax.scatter(translation[0], translation[2], -translation[1], color='blue', s=30, marker='x')
+#     else: # Find the nearest frame ID in key_trajectory_data
+#         nearest_frame_id = min(key_trajectory_data.keys(), key=lambda x: abs(x - loop))
+#         translation = key_trajectory_data[nearest_frame_id]
+#         ax.scatter(translation[0], translation[2], -translation[1], color='blue', s=30, marker='x')
+
+# # Set labels and title
+# ax.set_xlabel('X')
+# ax.set_ylabel('Z')
+# ax.set_zlabel('Y')
+# ax.set_title('Keyframe Trajectory')
+
+# Show plot
+#plt.show()
+
+print(f"precion = {precision(frame_numbers, gnd_truth)}")
+print(f"recall = {recall(frame_numbers, gnd_truth)}")
+
+import code
+code.interact(local=dict(globals(), **locals()))
